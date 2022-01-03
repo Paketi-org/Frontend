2156df4e6135939bda5df4e0f4d2b488003f5f17
Matevž Morato <matevz.morato@gmail.com>
2022-01-03 01:16:30 +0100

    base path

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def base_route():
+    return "Welcome", 200
+
 @app.after_request # blueprint can also be app~~
 def after_request(response):
     header = response.headers
